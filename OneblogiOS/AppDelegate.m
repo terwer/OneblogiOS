cc6294c77bd61716a1bd9f553dcf1261601dd826
Terwer Green <cbgtyw@gmail.com>
2015-07-09 01:34:35 +0800

    Clear project

--- OneblogiOS/AppDelegate.m
+++ OneblogiOS/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@"Launch success;");
+    
     return YES;
 }
 
