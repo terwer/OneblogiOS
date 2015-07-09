337e5c9b0404feb58c921cbdbfffa6c313abd19a
Terwer Green <cbgtyw@gmail.com>
2015-07-10 02:17:32 +0800

    Sync project

---
+++ OneblogiOSTests/OneblogiOSTests.m
@@ -0,0 +1,40 @@
+//
+//  iosappTests.m
+//  iosappTests
+//
+//  Created by chenhaoxiang on 14-10-13.
+//  Copyright (c) 2014年 oschina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface OneblogiOSTests : XCTestCase
+
+@end
+
+@implementation OneblogiOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
