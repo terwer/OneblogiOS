2011259acbeccac3031306b234029388c99fdd01
terwer <cbgtyw@gmail.com>
2015-07-09 17:24:03 +0800

    Add Category UIBarButtonItem+Badge

---
+++ OneblogiOS/UIBarButtonItem+Badge.h
@@ -0,0 +1,36 @@
+//
+//  UIBarButtonItem+Badge.h
+//  OneblogiOS
+//
+//  Created by Terwer Green on 15/7/9.
+//  Copyright (c) 2015年 Terwer Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBarButtonItem(Badge)
+
+@property (strong, nonatomic) UILabel *badge;
+
+// Badge value to be display
+@property (nonatomic) NSString *badgeValue;
+// Badge background color
+@property (nonatomic) UIColor *badgeBGColor;
+// Badge text color
+@property (nonatomic) UIColor *badgeTextColor;
+// Badge font
+@property (nonatomic) UIFont *badgeFont;
+// Padding value for the badge
+@property (nonatomic) CGFloat badgePadding;
+// Minimum size badge to small
+@property (nonatomic) CGFloat badgeMinSize;
+// Values for offseting the badge over the BarButtonItem you picked
+@property (nonatomic) CGFloat badgeOriginX;
+@property (nonatomic) CGFloat badgeOriginY;
+// In case of numbers, remove the badge when reaching zero
+@property BOOL shouldHideBadgeAtZero;
+// Badge has a bounce animation when value changes
+@property BOOL shouldAnimateBadge;
+
+
+@end
