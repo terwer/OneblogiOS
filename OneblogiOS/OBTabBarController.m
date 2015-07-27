7ad4d0776fa0cfcf9822debcb0067d946e5bb2ce
terwer <cbgtyw@gmail.com>
2015-07-27 16:53:49 +0800

    Update

--- OneblogiOS/OBTabBarController.m
+++ OneblogiOS/OBTabBarController.m
@@ -84,6 +84,36 @@
     
     [self addCenterButtonWithImage:[UIImage imageNamed:@"tabbar-more"]];
     
+    // 功能键相关
+    _optionButtons = [NSMutableArray new];
+    _screenHeight = [UIScreen mainScreen].bounds.size.height;
+    _screenWidth  = [UIScreen mainScreen].bounds.size.width;
+    _length = 60;        // 圆形按钮的直径
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    NSArray *buttonTitles = @[@"文字", @"相册", @"拍照", @"语音", @"扫一扫", @"找人"];
+    NSArray *buttonImages = @[@"tweetEditing", @"picture", @"shooting", @"sound", @"scan", @"search"];
+    int buttonColors[] = {0xe69961, 0x0dac6b, 0x24a0c4, 0xe96360, 0x61b644, 0xf1c50e};
+    
+    for (int i = 0; i < 6; i++) {
+        OptionButton *optionButton = [[OptionButton alloc] initWithTitle:buttonTitles[i]
+                                                                   image:[UIImage imageNamed:buttonImages[i]]
+                                                                andColor:[UIColor colorWithHex:buttonColors[i]]];
+        
+        optionButton.frame = CGRectMake((_screenWidth/6 * (i%3*2+1) - (_length+16)/2),
+                                        _screenHeight + 150 + i/3*100,
+                                        _length + 16,
+                                        _length + [UIFont systemFontOfSize:14].lineHeight + 24);
+        [optionButton.button setCornerRadius:_length/2];
+        
+        optionButton.tag = i;
+        optionButton.userInteractionEnabled = YES;
+        [optionButton addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTapOptionButton:)]];
+        
+        [self.view addSubview:optionButton];
+        [_optionButtons addObject:optionButton];
+    }
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -236,5 +266,7 @@
 }
 
 
+-(void)onTapOptionButton:(id)sender{
 
+}
 @end
