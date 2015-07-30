c829e75eb68b837ddebc8bd333c98f1046c34805
terwer <cbgtyw@gmail.com>
2015-07-30 17:08:51 +0800

    Add Eding page

--- OneblogiOS/PostEditViewController.m
+++ OneblogiOS/PostEditViewController.m
@@ -10,7 +10,11 @@
 #import "PostEditViewController.h"
 #import "PostDetailViewController.h"
 
-@interface PostEditViewController ()
+@interface PostEditViewController ()<UIScrollViewDelegate,UITextViewDelegate>
+
+@property (nonatomic, strong) UIScrollView          *scrollView;
+@property (nonatomic, strong) UIView                *contentView;
+@property (nonatomic, strong) PlaceholderTextView   *edittingArea;
 
 @end
 
@@ -20,8 +24,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    //工具栏
     self.navigationItem.title = @"撰写文章";
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(preview)];
+    
+    [self initSubViews];
+    [self setLayout];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,6 +46,44 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark 视图相关
+- (void)initSubViews
+{
+    _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    _scrollView.delegate = self;
+    _scrollView.showsHorizontalScrollIndicator = NO;
+    _scrollView.showsVerticalScrollIndicator   = NO;
+    _scrollView.scrollEnabled = YES;
+    _scrollView.bounces = YES;
+    [self.view addSubview:_scrollView];
+    
+    _contentView = [[UIView alloc] initWithFrame:_scrollView.bounds];
+    _contentView.userInteractionEnabled = YES;
+    [_scrollView addSubview:_contentView];
+    _scrollView.contentSize = _contentView.bounds.size;
+    
+    _edittingArea = [[PlaceholderTextView alloc] initWithPlaceholder:@"今天你发布博客了吗？"];
+    _edittingArea.delegate = self;
+    _edittingArea.placeholderFont = [UIFont systemFontOfSize:17];
+    _edittingArea.returnKeyType = UIReturnKeySend;
+    _edittingArea.enablesReturnKeyAutomatically = YES;
+    _edittingArea.scrollEnabled = NO;
+    _edittingArea.font = [UIFont systemFontOfSize:18];
+    _edittingArea.autocorrectionType = UITextAutocorrectionTypeNo;
+    [_contentView addSubview:_edittingArea];
+    
+
+}
+
+- (void)setLayout
+{
+    for (UIView *view in _contentView.subviews) {view.translatesAutoresizingMaskIntoConstraints = NO;}
+     NSDictionary *views = NSDictionaryOfVariableBindings(_edittingArea, _contentView);
+    
+    [_contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-8-[_edittingArea]-8-|" options:0 metrics:nil views:views]];
+}
+
 
 /**
  *  预览
