b1843454179d046ccea6b6f16dca7023f49c126b
Terwer Green <cbgtyw@gmail.com>
2015-07-30 21:24:18 +0800

    Comment utils and config

--- OneblogiOS/Config.h
+++ OneblogiOS/Config.h
@@ -18,5 +18,17 @@
  */
 + (BOOL)getMode;
 
+/**
+ *  获取MetaWeblog API的授权信息
+ *
+ *  @return ApiInfo
+ */
 +(ApiInfo *)getAuthoizedApiInfo;
+
+/**
+ *  是否启用高级API（注意：如果启用了，需要服务端支持，https://github.com/terwer/SDFeedParser）
+ *
+ *  @return 高级API开启状态
+ */
++(BOOL)isAnvancedAPIEnable;
 @end
